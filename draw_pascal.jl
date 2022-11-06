0d1b766a9eab34e0a03742760c9bd51d47780116
thisisnotdevin <devin1299@outlook.com>
2022-11-06 17:14:53 -0500

    added minmaxmedian

--- draw_pascal.jl
+++
@@ -1,2 +0,0 @@
-# a function that takes an integer n as a parameter and prints the first n rows of the Pascal's triangle.
-
--- min_max_median.jl
+++ min_max_median.jl
@@ -1,15 +1,16 @@
+using Statistics
 function min_max_median(lst)
+
     sort!(lst)
     size = length(lst)
     min = lst[1]
     max = lst[size]
-    median = 0
-    if (size % 2 != 0)
-        median = lst[size/2]
-    end    
-    if (size % 2 == 0)
+    
+    mid = mean(lst)
 
-        median = (lst[size-1/2] + lst[size/2]) / 2.0
-    end  
-    return min, max, median
+
+
+    return min, max, mid
+
+    
 end
